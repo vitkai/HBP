0cb396dc6c8b51ce15e747ae688f4828fbe2cac2
Corvit Chr <corvit.christian@gmail.com>
2020-03-15 12:14:31 +0300

    HBP: hbpsite: Currencies update added|Need to check CCy description when adding new record

--- hbpsite/hbpapp/db_updates.py
+++ hbpsite/hbpapp/db_updates.py
@@ -7,7 +7,7 @@ import __main__
 import pandas as pd
 
 from os import path
-from .models import Category #, Transactions, CCY, Document
+from .models import Category, CCY #, Transactions, Document
 
 # own function to setup logger
 from .my_logger import logging_setup
@@ -22,12 +22,18 @@ def general_init():
     logger.debug("Full path: {0} | filename: {1}".format(full_path, filename))
     
 
-def check_categories(catl):
+def check_categories(df):
     """
-    receives parameters:
-        catl - categories list
+    receives parameters: 
+        df - DataFrame to work with
     returns check status string
     """
+    # get a list of unique values form 'Category' column
+    catl = set(df['Category'].tolist())
+    
+    msg = f"Categories list:\n{catl}"
+    logger.debug(msg)
+    
     res_status = ''
     
     for cat in catl:
@@ -48,6 +54,37 @@ def check_categories(catl):
     return res_status
 
 
+def check_currencies(df):
+    """
+    receives parameters: 
+        df - DataFrame to work with
+    returns check status string
+    """
+    # get a list of unique values form 'CCY' column
+    catl = set(df['CCY'].tolist())
+    
+    msg = f"Categories list:\n{catl}"
+    logger.debug(msg)
+    
+    res_status = ''
+    
+    for cat in catl:
+        if CCY.objects.filter(name=cat).exists():
+            msg = f"Currency: {cat} already exists in db"
+            logger.debug(msg)
+            res_status = res_status + f'| {msg}'
+        else:
+            new_cat = CCY.objects.create(name=cat)
+            msg = f"Adding CCY: {new_cat}"
+            logger.debug(msg)
+            res_status = res_status + f'| {msg}'
+            new_cat.save()
+    
+    if res_status == '':
+        res_status = 'No db updates'
+    
+    return res_status
+
 
 def proc_db_import(df_to_proc):
     general_init()
@@ -56,15 +93,8 @@ def proc_db_import(df_to_proc):
     print("\nThat's all folks")
 
     result = 'proc_db_import says hello'
-    
-    # get a list of unique values form 'Category' column
-    # cats = get_uniq_col_values(df_to_proc)
-    cats = set(df_to_proc['Category'].tolist())
-    
-    msg = f"Categories list:\n{cats}"
-    logger.debug(msg)
-    
-    result = result + ' | ' + check_categories(cats)
+    result = result + ' | ' + check_categories(df_to_proc)
+    result = result + ' | ' + check_currencies(df_to_proc)
 
     return result
 
